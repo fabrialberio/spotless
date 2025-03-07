9494253cea771beb3bdf5e49cffb86de7302637a
fabrialberio <->
2025-03-07 13:48:40 +0100

    Implement spotipy wrappers

--- extractor.py
+++
@@ -1,9 +0,0 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-scope = "user-library-read"
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
-if __name__ == "__main__":
-    print(sp.current_user_playlists())
