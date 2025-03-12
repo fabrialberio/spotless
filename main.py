48bf4ef252513cce78a429b9b73845e91fbf86cd
fabrialberio <->
2025-03-12 10:03:05 +0100

    Add timer

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-from unspotify import UnspotifyPlaylist, UnspotifyYTDownloader
+from downloaders import UnspotifyThreadedDownloader
+from unspotify import UnspotifyPlaylist
 
 if __name__ == "__main__":
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=""))
@@ -10,6 +13,13 @@ if __name__ == "__main__":
     playlist = UnspotifyPlaylist.from_url(sp, playlist_url)
 
     print(f"Downloading songs from «{playlist.name}»...")
+    start_time = time.time()
 
-    downloader = UnspotifyYTDownloader()
-    downloader.download_playlist(playlist)
+    downloader = UnspotifyThreadedDownloader()
+    downloader.download_tracks(
+        playlist.name,
+        list(playlist),
+        lambda: print(
+            f"Download completed in {time.time() - start_time:.2f} seconds."
+        ),
+    )
